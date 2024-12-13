dc684d421d5a3c1be4cfecb22e58660809a84401
Herman Ceaser <ceaserbanks09@gmail.com>
2024-12-13 15:07:55 +0300

    created transfer script

--- airdrop.ts
+++ airdrop.ts
@@ -3,13 +3,14 @@ import wallet from "./dev-wallet.json"
 
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
 console.log(keypair.publicKey.toBase58());
-// const connection = new Connection("https://api.devnet.solana.com");
-const connection = new Connection("http://127.0.0.1:8899");
+const connection = new Connection("https://api.devnet.solana.com");
+// const connection = new Connection("http://127.0.0.1:8899");
 (
     async () => {
         try {
-            const txhash = await connection.requestAirdrop(keypair.publicKey, 40 * LAMPORTS_PER_SOL);
-            console.log(`Success! Check out your TX here: https://explorer.solana.com/tx/${txhash}?cluster=custom&customUrl=http://localhost:8899`);
+            const txhash = await connection.requestAirdrop(keypair.publicKey, 2 * LAMPORTS_PER_SOL);
+            // console.log(`Success! Check out your TX here: https://explorer.solana.com/tx/${txhash}?cluster=custom&customUrl=http://localhost:8899`);
+            console.log(`Success! Check out your TX here: https://explorer.solana.com/tx/${txhash}?cluster=devnet`);
         } catch (e) {
             console.error(`Oops, something went wrong: ${e}`)
         }
